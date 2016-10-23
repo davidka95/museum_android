38b0c1b01700228753dc169425e3d119505cf403
Zay <gzaymus@gmail.com>
2016-10-23 16:56:33 +0200

    On the navigation bar Museum has been renamed to Exhibition

--- app/src/main/java/hu/bme/aut/exhibitionexplorer/MainActivity.java
+++ app/src/main/java/hu/bme/aut/exhibitionexplorer/MainActivity.java
@@ -2,17 +2,13 @@ package hu.bme.aut.exhibitionexplorer;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -56,7 +52,7 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.nav_muesum) {
+        if (id == R.id.nav_exhibition) {
             showFragment(new ExhibitionFragment(), ExhibitionFragment.TAG);
         } else if (id == R.id.nav_favorite) {
 
